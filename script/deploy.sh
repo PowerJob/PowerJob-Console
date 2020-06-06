126d4ccaecda52fd7aadaf90c5261dc0c0e7283e
tjq <tengjiqi@gmail.com>
2020-06-06 21:53:59 +0800

    [fix] fix the baseUrl problem in java build mode

--- script/deploy.sh
+++ script/deploy.sh
@@ -1,7 +1,8 @@
-# 构建项目
-npm run build
-# 删除旧文件
+#!/bin/sh
+
+# 在根目录运行该脚本文件，自动替换 oms-server 的静态文件
+
+npm run build_spring
 rm -rf ../OhMyScheduler/oh-my-scheduler-server/src/main/resources/static/*
-# 拷贝新文件
 mv dist/* ../OhMyScheduler/oh-my-scheduler-server/src/main/resources/static/
 
