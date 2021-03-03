6351126a131863597353aeba7126895c5efddf1e
赵秀全 <zhaoxiuquan@cvte.com>
2021-03-03 15:08:01 +0800

    feat: 重构任务详情

--- src/components/common/InstanceDetail.vue
+++ src/components/common/InstanceDetail.vue
@@ -1,76 +1,110 @@
 <template>
-  <div>
+  <div class="power-job-panl">
     <el-row>
-      <el-col :offset="20">
+      <div class="power-job-button">
         <el-button type="primary" @click="fetchInstanceDetail">{{$t('message.refresh')}}</el-button>
-      </el-col>
+      </div>
     </el-row>
-    <el-row>
-      <el-col :span="24">
-        {{$t('message.instanceId')}}:
-        <span class="title">{{instanceId}}</span>
-      </el-col>
-    </el-row>
-    <el-row style="margin-top:-20px">
-      <el-col :span="8">
-        {{$t('message.status')}}:
-        <span class="title">{{this.common.translateInstanceStatus(instanceDetail.status)}}</span>
-      </el-col>
-      <el-col :span="16">
-        {{$t('message.runningTimes')}}：
-        <span class="title">{{instanceDetail.runningTimes}}</span>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="24">
-        {{$t('message.taskTrackerAddress')}}:
-        <span class="title">{{instanceDetail.taskTrackerAddress}}</span>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="24">
-        {{$t('message.expectedTriggerTime')}}:
-        <span class="title">{{instanceDetail.expectedTriggerTime}}</span>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="8">
-        {{$t('message.startTime')}}:
-        <span class="title">{{instanceDetail.actualTriggerTime}}</span>
-      </el-col>
-      <el-col :span="8">
-        {{$t('message.finishedTime')}}:
-        <span class="title">{{instanceDetail.finishedTime}}</span>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="24">
-        {{$t('message.instanceParams')}}:
-        <span class="title">{{instanceDetail.instanceParams}}</span>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="24">
-        {{$t('message.result')}}:
-        <span class="title">{{instanceDetail.result}}</span>
-      </el-col>
-    </el-row>
-    <el-row id="taskDetail">
-      {{$t('message.subTaskInfo')}}:
-      <span class="title">{{instanceDetail.taskDetail}}</span>
-    </el-row>
-    <el-divider content-position="center">{{$t('message.secondlyJobHistory')}}:</el-divider>
-    <el-row>
-      <el-table :data="instanceDetail.subInstanceDetails" style="width: 100%">
-        <el-table-column prop="subInstanceId" :label="$t('message.subInstanceId')" width="120" />
-        <el-table-column prop="startTime" :label="$t('message.startTime')" width="160" />
-        <el-table-column prop="finishedTime" :label="$t('message.finishedTime')" width="160" />
-        <el-table-column :label="$t('message.status')" width="160">
-          <template slot-scope="scope">{{common.translateInstanceStatus(scope.row.status)}}</template>
-        </el-table-column>
-        <el-table-column prop="result" :label="$t('message.result')" />
-      </el-table>
-    </el-row>
+    <div class="power-job-info" :style="{'width': fixedWidth ? fixedWidth : '100%'}">
+      <el-card>
+        <el-row class="job-detail-text">
+          <el-col :span="24">
+            <span class="power-job-text">{{$t('message.instanceId')}}:</span>
+            <span class="title">{{instanceId}}</span>
+          </el-col>
+        </el-row>
+        <el-row class="job-detail-text">
+          <el-col :span="24">
+            <span class="power-job-text">{{$t('message.status')}}:</span>
+            <span
+              class="title"
+            >{{this.common.translateInstanceStatus(instanceDetail.status)}}</span>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24">
+            <span class="power-job-text">{{$t('message.runningTimes')}}:</span>
+            <span class="title">{{instanceDetail.runningTimes}}</span>
+          </el-col>
+        </el-row>
+        <el-row class="job-detail-text">
+          <el-col :span="24">
+            <span class="power-job-text">{{$t('message.taskTrackerAddress')}}:</span>
+            <span
+              class="title"
+            >{{instanceDetail.taskTrackerAddress}}</span>
+          </el-col>
+        </el-row>
+        <el-row class="job-detail-text">
+          <el-col :span="24">
+            <span class="power-job-text">{{$t('message.expectedTriggerTime')}}:</span>
+            <span
+              class="title"
+            >{{instanceDetail.expectedTriggerTime}}</span>
+          </el-col>
+        </el-row>
+        <el-row class="job-detail-text">
+          <el-col :span="24">
+            <span class="power-job-text">{{$t('message.startTime')}}:</span>
+            <span class="title">{{instanceDetail.actualTriggerTime}}</span>
+          </el-col>
+        </el-row>
+        <el-row>
+          <el-col :span="24">
+            <span class="power-job-text">{{$t('message.finishedTime')}}:</span>
+            <span class="title">{{instanceDetail.finishedTime}}</span>
+          </el-col>
+        </el-row>
+        <el-row class="job-detail-text">
+          <el-col :span="24">
+            <span class="power-job-text">{{$t('message.nodeParamsName')}}:</span>
+            <span class="title">{{instanceDetail.jobParams}}</span>
+          </el-col>
+        </el-row>
+        <el-row class="job-detail-text">
+          <el-col :span="24">
+            <span class="power-job-text">{{$t('message.instanceParams')}}:</span>
+            <span class="title">{{instanceDetail.instanceParams}}</span>
+          </el-col>
+        </el-row>
+        <el-row class="job-detail-text">
+          <el-col :span="24">
+            <div class="power-job-result">
+              <span class="power-job-text">{{$t('message.result')}}:</span>
+              <el-popover width="400" placement="right" trigger="click">
+                <div class="power-job-content-slot">
+                  {{instanceDetail.result}}
+                </div>
+                <span class="power-job-content" slot="reference" :style="{width: fixedWidth ? `${fixedWidth - 200}px` : '400px'}">{{instanceDetail.result}}</span>
+                <!-- <i class="el-icon-chat-dot-square result" slot="reference"></i> -->
+              </el-popover>
+            </div>
+          </el-col>
+        </el-row>
+        <slot></slot>
+        <el-row class="job-detail-text" id="taskDetail" v-if="instanceDetail.taskDetail">
+          <span class="power-job-text">{{$t('message.subTaskInfo')}}:</span>
+          <span class="title">{{instanceDetail.taskDetail}}</span>
+        </el-row>
+      </el-card>
+    </div>
+
+    <el-divider content-position="center" v-if="instanceDetail.subInstanceDetails">{{$t('message.secondlyJobHistory')}}</el-divider>
+    <div class="power-job-info" v-if="instanceDetail.subInstanceDetails" :style="{'width': fixedWidth ? fixedWidth : '100%'}">
+      <el-card>
+        <el-row>
+          <el-table :data="instanceDetail.subInstanceDetails" style="width: 100%">
+            <el-table-column :show-overflow-tooltip="true" prop="subInstanceId" :label="$t('message.subInstanceId')" width="120" />
+            <el-table-column :show-overflow-tooltip="true" prop="startTime" :label="$t('message.startTime')" width="160" />
+            <el-table-column :show-overflow-tooltip="true" prop="finishedTime" :label="$t('message.finishedTime')" width="160" />
+            <el-table-column :show-overflow-tooltip="true" :label="$t('message.status')" width="140">
+              <template slot-scope="scope">{{common.translateInstanceStatus(scope.row.status)}}</template>
+            </el-table-column>
+            <el-table-column :show-overflow-tooltip="true" prop="result" :label="$t('message.result')" />
+          </el-table>
+        </el-row>
+      </el-card>
+    </div>
   </div>
 </template>
 
@@ -78,7 +112,7 @@
 export default {
   name: "InstanceDetail",
   // 数据传递
-  props: ["instanceId"],
+  props: ["instanceId", "fixedWidth"],
   data() {
     return {
       instanceDetail: {}
@@ -94,15 +128,71 @@ export default {
   mounted() {
     console.log("using InstanceId: " + this.instanceId);
     this.fetchInstanceDetail();
+  },
+  watch: {
+    instanceId() {
+      this.fetchInstanceDetail();
+    }
   }
 };
 </script>
 
 <style scoped>
+*,
+*::after,
+*::before {
+  box-sizing: border-box;
+}
 .title {
   display: inline-block;
-  margin: 5px 0;
-  font-size: 16px;
+  /* margin: 5px 0; */
+  font-size: 14px;
   font-weight: bold;
+}
+.power-job-button {
+  display: flex;
+  width: 100%;
+  justify-content: flex-end;
+  padding-top: 5px ;
+  padding-right: 5px;
+}
+.power-job-info {
+  padding: 5px 5px;
+}
+.power-job-text {
+  display: inline-block;
+  width: 148px;
+  text-align: right;
+  margin-right: 4px;
+  font-size: 14px;
+}
+.result:hover {
+  transition: 0.5s;
+  color: #52AEFF;
+}
+.power-job-result {
+  display: flex;
+  align-items: center;
+}
+.power-job-content-slot {
+  max-height: 300px;
+  overflow-y: scroll;
+}
+.power-job-content {
+  /* width: 205px; */
+  white-space:nowrap;
+  overflow:hidden;
+  text-overflow:ellipsis;
+  display: inline-block;
+  font-size: 14px;
+  /* line-height: 20px; */
+}
+</style>
+<style>
+.el-tooltip__popper.is-dark {
+  max-width: 50%;
+  word-wrap: break-word;
+  white-space: normal;
+  word-break: break-all;
 }
 </style>
